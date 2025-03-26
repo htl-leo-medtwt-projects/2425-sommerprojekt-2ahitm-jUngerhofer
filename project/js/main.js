94a146a8f9a40014e5797d9b9015c07d7a7052d3
Ungerhofer Julian <j.ungerhofer@HTBLALeonding.onmicrosoft.com>
2025-03-26 13:37:15 +0100

    js, generate how to page + styling

---
+++ project/js/main.js
@@ -0,0 +1,7 @@
+function loadHowTo(){
+    document.getElementsByClassName('container')[0].style.display = "none;";
+
+    let newContent = "";
+    document.getElementById('howToPage').innerHTML = newContent;
+}
+
