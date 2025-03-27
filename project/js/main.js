82b57031a77e3f1368dd7fab43e68e717043abc1
Ungerhofer Julian <j.ungerhofer@HTBLALeonding.onmicrosoft.com>
2025-03-27 13:37:37 +0100

    HowToPage + Dokumentation and Styling

--- project/js/main.js
+++ project/js/main.js
@@ -1,7 +1,15 @@
 function loadHowTo(){
-    document.getElementsByClassName('container')[0].style.display = "none;";
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('startGame').style.display = 'none';
+    document.getElementById('howToPage').style.display = 'flex';
 
-    let newContent = "";
-    document.getElementById('howToPage').innerHTML = newContent;
+    document.body.style.backgroundImage = 'url(img/howToPlay.jpg)';
 }
 
+
+function closeHowTo(){
+    document.getElementById('container').style.display = 'flex';
+    document.getElementById('startGame').style.display = 'block';
+    document.getElementById('howToPage').style.display = 'none';
+    document.body.style.backgroundImage = 'url(img/StartingScreen.jpg)';
+}
