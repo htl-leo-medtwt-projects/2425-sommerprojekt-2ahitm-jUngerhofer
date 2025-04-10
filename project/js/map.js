1ed1a9c3155276484b0fbc3a51bffb39c35c51b2
Ungerhofer Julian <j.ungerhofer@HTBLALeonding.onmicrosoft.com>
2025-04-10 12:09:23 +0200

    Headerbar for the game (Shop, Map, Character)

---
+++ project/js/map.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', () => {
+      document.querySelectorAll('.nav-item').forEach(i => i.classList.remove('active'));
+      item.classList.add('active');
+    });
+  });
+  
